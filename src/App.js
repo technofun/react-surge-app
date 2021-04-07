38fe6cad5386a60de72c801f6ff1f2673f046414
NuMan MirXa <35162466+technofun@users.noreply.github.com>
2021-04-07 21:22:33 +0500

    changed nodeversion in yml file

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Numan ijaz
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
+<h1>Hello from Numan ijaz</h1>
     </div>
   );
 }
